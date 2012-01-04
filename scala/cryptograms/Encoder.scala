ea2e3e1b0d2d1b00900558425a4f8d1b85bf8f0d
Nicholas McAvoy <nicholas.mcavoy@gmail.com>
2012-01-03 22:06:47 -0500

    Using Scala OO
    Using values instantiated with Encoder class. Removing parameters to
    functions accordingly.

--- scala/cryptograms/Encoder.scala
+++ scala/cryptograms/Encoder.scala
@@ -2,29 +2,28 @@ package cryptograms
 import scala.util.Random
 import scala.collection.immutable.Map
 
-class Encoder(toEncipher: List[Char], gen: Random) {
-	val alphabet = 'a' to 'z' toList
-	val alphabetUL = ('A' to 'Z').toList ::: alphabet
+class Encoder(chars: List[Char], gen: Random) {
+	private val alphabet = 'a' to 'z' toList
+	private val alphabetUL = ('A' to 'Z').toList ::: alphabet
 	
-	def createCipher(chars: List[Char], gen: Random): Map[Char, Char] = {
+	def createCipher: Map[Char, Char] = {
 	  val low = chars map(c => c toString() toLowerCase() charAt(0)) distinct
-	  val res = aux(low, Map.empty, gen)
+	  val res = aux(low, Map.empty)
 	  res match {
-	    case None    => return createCipher(low, new Random)
+	    case None    => return createCipher
 	    case Some(m) => return m 
 	  }
 	}
 	
-	private def aux(cs: List[Char], m: Map[Char, Char], gen: Random): Option[Map[Char, Char]] = cs match {
+	private def aux(cs: List[Char], m: Map[Char, Char]): Option[Map[Char, Char]] = cs match {
 	  case Nil 	   => Some(m)
 	  case c :: cs => 
 	    val avail = alphabet.toSet.diff(m.values.toSet).toList.filter(cp => c != cp)
-	 //   println("map is " + m.toString)
 	  	val nc = {n: Int => avail apply gen.nextInt(n)}
 	  	if (0 == avail.length)
 	  	  None
 	  	else 
-	  	  aux(cs, (m + (c -> nc(avail.length))), gen)
+	  	  aux(cs, (m + (c -> nc(avail.length))))
 	}
 	
 }
--- scala/cryptograms/Main.scala
+++ scala/cryptograms/Main.scala
@@ -6,7 +6,6 @@ object Main {
 	def main(args: Array[String]): Unit = {
 	  val alph = ('a' to 'z') toList
 	  val enc = new Encoder(alph, new Random)
-	  val str = enc.createCipher(alph, new Random).toString
-	  println(str)
+	  println(enc.createCipher.toString)
 	}
 }
