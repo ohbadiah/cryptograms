018ded20b390485d3419425ccfd87a32464f0af2
Nicholas McAvoy <nicholas.mcavoy@gmail.com>
2012-01-04 23:48:59 -0500

    Scala enciphers
    It just prints the result out to STDOUT now. IO does not seem to be
    Scala's strong suit.

--- scala/cryptograms/Encoder.scala
+++ scala/cryptograms/Encoder.scala
@@ -7,20 +7,27 @@ class Encoder(chars: Set[Char], gen: Random) {
 	private val alphabet = ('a' to 'z').toSet
 	private val alphabetUL = ('A' to 'Z').toSet & alphabet
 	
+	def toLower(c: Char): Char = {return c toString() toLowerCase() charAt(0)}
+	def toUpper(c: Char): Char = {return c toString() toUpperCase() charAt(0)}
+	
 	def createCipher: Map[Char, Char] = {
-	  val low = chars map(c => c toString() toLowerCase() charAt(0))
+	  val low = chars map(toLower)
 	  val res = aux(low, Map.empty)
+	  
 	  res match {
 	    case None    => return createCipher
-	    case Some(m) => return m 
+	    case Some(m) => return m.foldRight(Map.empty[Char, Char])(
+	        (t: (Char, Char), m: Map[Char, Char]) => t match {
+	          case s@(k, v) => m + s + ((toUpper(k), toUpper(v)))
+	        })
 	  }
 	}
 	
 	private def aux(chars: Set[Char], m: Map[Char, Char]): Option[Map[Char, Char]] = chars.toList match {
 	  case Nil 	   => Some(m)
 	  case c :: cs => 
-	    val avail = (alphabet &~ (m.values.toSet & Set(c))).toList
-	  	val nc = {n: Int => avail apply gen.nextInt(n)}
+	    val avail = (alphabet &~ (m.values.toSet ++ Set(c))).toList
+	  	val nc = {n: Int => avail(gen.nextInt(n))}
 	  	if (0 == avail.length)
 	  	  None
 	  	else 
@@ -28,7 +35,8 @@ class Encoder(chars: Set[Char], gen: Random) {
 	}
 	
 	def enCipher(ciph: Map[Char, Char])(text: String): String = {
-	  return text.toList map(c => ciph.getOrElse(c, c)) toString
+	  val lst = text.toList map(c => ciph.getOrElse(c, c))
+	  return lst.foldRight("")((ch: Char, str: String) => ch + str)
 	}
 	
 }
--- scala/cryptograms/Main.scala
+++ scala/cryptograms/Main.scala
@@ -1,11 +1,22 @@
 package cryptograms
 
 import scala.util.Random
+import scala.io.Source
 
 object Main {
 	def main(args: Array[String]): Unit = {
-	  val alph = ('a' to 'z') toSet
-	  val enc = new Encoder(alph, new Random)
-	  println(enc.createCipher.toString)
+	  if (args.length == 0) println("No input file given.")
+	  else {
+	    val alph = ('a' to 'z') toSet
+	    val enc = new Encoder(alph, new Random)
+	    val ciph = enc.createCipher
+	    val encipherer = enc.enCipher(ciph)_
+	    val file = Source.fromFile(args(0))
+	    
+	    for (line <- file.getLines())
+	      println(encipherer(line))
+	    file.close
+	  }
+	  
 	}
 }
